e96fb869ce2105fb17fcce5b83825b757944d13f
Stherfany <stherfanyalves.sa@gmail.com>
2019-04-11 10:13:26 -0300

    Adicionando Aula-3

---
+++ aulas/aula-3/aula3.cpp
@@ -0,0 +1,14 @@
+// Entrada e saída simples em c++
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int idade;
+    cout << "Hello World\nAbilio Castro" << endl;
+    cout << "Digite sua idade: ";
+    cin >> idade;
+    cout << "Daqui a 15 anos voce tem: " << idade + 15 << endl;
+    return 0;
+}
